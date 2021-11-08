4079f7e9bfad2efa0f76d2455a4478acdb0874c6
Edimichael Lima <edimichael.lima@gmail.com>
2021-11-08 12:40:12 -0300

    Layout das paginas index e login

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/index")
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
